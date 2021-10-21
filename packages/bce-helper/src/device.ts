6cf48d9ad4bedf19a82c1b155ea6b409c65ce492
liuxinya <liuxinya@baidu.com>
2021-10-21 15:53:04 +0800

    增加了 pc、移动、ios、android、rem设置工具
    Change-Id: I6dc1a0199862a906c8ddaa17b450311a2ee96ae7

--- packages/bce-helper/src/device.ts
+++ packages/bce-helper/src/device.ts
@@ -5,6 +5,21 @@ export function isMobile() {
     return Boolean(navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i));
 }
 
+export function isPc() {
+    return !isMobile() && !isIos() && !isAndroid();
+}
+
+export function isIos(): boolean {
+    const u = navigator.userAgent;
+    return Boolean(!!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/))
+}
+
+export function isAndroid(): boolean {
+    const u = navigator.userAgent;
+    return u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+}
+
+
 export function isWeChat() {
    return /MicroMessenger/i.test(window.navigator.userAgent); 
 }
---
+++ packages/bce-helper/src/rem.ts
@@ -0,0 +1,11 @@
+export function setRem(baseWidth = 375) {
+    const dpr = window.devicePixelRatio;
+    const currentWidth = document.documentElement.clientWidth;
+    let remSize = 0;
+    let scale = 0;
+    scale = currentWidth / baseWidth;
+    remSize = baseWidth / 10;
+    remSize = remSize * scale;
+    document.documentElement.style.fontSize = `${remSize}px`;
+    document.documentElement.setAttribute('data-dpr', `${dpr}`);
+}
